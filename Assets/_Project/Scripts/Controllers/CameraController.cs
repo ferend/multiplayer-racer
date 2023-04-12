3f2ea007c1a1b7a81bec12dd5776b8e0844ba982
ferend <dalcikferhateren@gmail.com>
2023-04-12 22:04:44 +0300

    move camera to root obj

--- Assets/_Project/Scripts/Controllers/CameraController.cs
+++ Assets/_Project/Scripts/Controllers/CameraController.cs
@@ -7,21 +7,20 @@ namespace Controllers
     public class CameraController : MonoBehaviour
     {
 
-        [SerializeField] private Camera _mainCamera;
+        [SerializeField] private Camera _playerCamera;
         [SerializeField] private Transform _target;
         [SerializeField] private Vector3 _offset;
 
-        private void LateUpdate()
-        {
-            Follow();
-        }
+
+        
 
         private void Follow()
         {
             if (_target == null || _isCameraStopped) return;
-            _mainCamera.transform.position = new Vector3(Mathf.Lerp(_mainCamera.transform.position.x,
-                _target.transform.position.x, Constants.cameraPosXLerpSpeed * Time.deltaTime), _target.transform.position.y + _offset.y, _target.transform.position.z + _offset.z);
+            _playerCamera.transform.position = new Vector3(Mathf.Lerp(_playerCamera.transform.position.x,
+            _target.transform.position.x, Constants.cameraPosXLerpSpeed * Time.deltaTime), _target.transform.position.y + _offset.y, _target.transform.position.z + _offset.z);
         }
+
         private bool _isCameraStopped;
         public void StopCamera() => _isCameraStopped = true;
     }
